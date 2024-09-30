af1a7d633e30158876223c9cb29a5c72f7d3d6c2
Pranav Praveen <brawlclicks58@gmail.com>
2024-09-30 14:21:16 +0530

    updated footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,7 +105,7 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
+        <a href="https://github.com/twistedvision518/portfoliotest">
           <div>Designed &amp; Built by Pranav Praveen</div>
 
           {githubInfo.stars && githubInfo.forks && (
